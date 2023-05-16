4b9dab831736b174e3a79ff8debb493b83dbe387
이준혁 <hol22mol22@gmail.com>
2023-05-16 16:05:36 +0900

    chore : set fortune domain frame

--- src/main/java/com/ssafy/luminous/fortune/domain/Fortune.java
+++ src/main/java/com/ssafy/luminous/fortune/domain/Fortune.java
@@ -1,4 +1,21 @@
 package com.ssafy.luminous.fortune.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Fortune {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
