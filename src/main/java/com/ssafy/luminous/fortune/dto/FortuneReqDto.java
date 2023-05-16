6bb094840b00fed11f93755a0fdfb0baa77b8d2a
이준혁 <hol22mol22@gmail.com>
2023-05-16 17:14:45 +0900

    feat : get fortune Info

--- src/main/java/com/ssafy/luminous/fortune/dto/FortuneReqDto.java
+++ src/main/java/com/ssafy/luminous/fortune/dto/FortuneReqDto.java
@@ -1,4 +1,12 @@
 package com.ssafy.luminous.fortune.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FortuneReqDto {
+    private Long id;
 }
