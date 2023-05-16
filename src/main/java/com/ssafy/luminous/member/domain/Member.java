b189470914fab1eafc23598a71e7fbf4e3d269c3
Chae <eqholic1125@gmail.com>
2023-05-16 15:44:40 +0900

    feat: Member 도메인 작성

--- src/main/java/com/ssafy/luminous/member/domain/Member.java
+++ src/main/java/com/ssafy/luminous/member/domain/Member.java
@@ -1,4 +1,38 @@
 package com.ssafy.luminous.member.domain;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor(force = true)
 public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    @NotNull
+    private String userId;
+
+    @NotNull
+    private String userPassword;
+
+    @NotNull
+    private String userName;
+
+    @NotNull
+    private String birth;
+
+    @NotNull
+    private String constellationId;
+
 }
