fc8fe97e0b473080f7610cd0b37bcd8c170ddc6c
이준혁 <hol22mol22@gmail.com>
2023-05-16 16:19:58 +0900

    chore : set constellation domain

--- src/main/java/com/ssafy/luminous/constellation/domain/Constellation.java
+++ src/main/java/com/ssafy/luminous/constellation/domain/Constellation.java
@@ -1,4 +1,24 @@
 package com.ssafy.luminous.constellation.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Constellation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
