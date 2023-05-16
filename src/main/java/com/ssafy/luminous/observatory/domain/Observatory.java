5ff00164bd9fd8a2584a8b525a921f90a42a8f75
이준혁 <hol22mol22@gmail.com>
2023-05-16 17:40:15 +0900

    chore : set domain Observatory

--- src/main/java/com/ssafy/luminous/observatory/domain/Observatory.java
+++ src/main/java/com/ssafy/luminous/observatory/domain/Observatory.java
@@ -1,4 +1,42 @@
 package com.ssafy.luminous.observatory.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Observatory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String observatoryName;
+
+    private Boolean isObservable;
+
+    private Boolean hasExhibition;
+
+    private Boolean hasLodgment;
+
+    private Boolean hasPlaintarium;
+
+    private String homePage;
+
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String address;
+
+    private Double rate;
+
+
+
+
 }
