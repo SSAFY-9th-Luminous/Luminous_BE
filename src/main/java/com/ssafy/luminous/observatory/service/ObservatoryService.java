800b88943de67ae8b63ac1b75bf08178a4c9c6b4
jun <sunjm135@naver.com>
2023-05-18 17:58:33 +0900

    chore : Observatory API 일부 기능 구현 완료

--- src/main/java/com/ssafy/luminous/observatory/service/ObservatoryService.java
+++ src/main/java/com/ssafy/luminous/observatory/service/ObservatoryService.java
@@ -1,4 +1,28 @@
 package com.ssafy.luminous.observatory.service;
 
+import com.ssafy.luminous.observatory.domain.Observatory;
+import com.ssafy.luminous.observatory.dto.ObservatoryReqDto;
+import com.ssafy.luminous.observatory.repository.ObservatoryRepository;
+import com.ssafy.luminous.place.domain.Place;
+import com.ssafy.luminous.place.dto.PlaceListResDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ObservatoryService {
+
+    private final ObservatoryRepository observatoryRepository;
+
+    public List<Observatory> getObservatoryList(String keyword){
+        List<Observatory> observatorys;
+
+        observatorys= observatoryRepository.findByAddressStartsWith(keyword);
+
+
+        return observatorys;
+    }
 }
