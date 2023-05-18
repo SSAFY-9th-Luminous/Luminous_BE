c5f7f274a7cb5beaca8fd1c3e03448e3c464b7b4
이준혁 <hol22mol22@gmail.com>
2023-05-18 14:00:52 +0900

    chore : create ContesllationDetail Domain

---
+++ src/main/java/com/ssafy/luminous/constellation/domain/ConstellationDetail.java
@@ -0,0 +1,45 @@
+package com.ssafy.luminous.constellation.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConstellationDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String contentsName;
+
+    @Column(nullable = false)
+    private String ScientificName;
+
+    @Column(nullable = false)
+    private String engName;
+
+    @Column(nullable = false)
+    private Date meridianDate;
+
+    @Column(nullable = false)
+    private Integer amountOfStars;
+
+    @Lob
+    @Column(nullable = false)
+    private String contentsDescription;
+
+    private String img;
+
+    @OneToOne
+    @JoinColumn(name ="id")
+    Constellation constellation;
+
+}
