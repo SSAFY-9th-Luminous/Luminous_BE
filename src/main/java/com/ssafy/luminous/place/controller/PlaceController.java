7f03ad1f5d7acb9b1288ebb239e49bede71fee9c
이준혁 <hol22mol22@gmail.com>
2023-05-19 16:00:33 +0900

    refactor : place controller 봉투패턴 적용, place service exception 처리

--- src/main/java/com/ssafy/luminous/place/controller/PlaceController.java
+++ src/main/java/com/ssafy/luminous/place/controller/PlaceController.java
@@ -2,6 +2,7 @@ package com.ssafy.luminous.place.controller;
 
 import com.ssafy.luminous.config.BaseException;
 import com.ssafy.luminous.config.BaseResponse;
+import com.ssafy.luminous.config.BaseResponseStatus;
 import com.ssafy.luminous.place.domain.Place;
 import com.ssafy.luminous.place.dto.PlaceListResDto;
 import com.ssafy.luminous.place.dto.PlacePostReqDto;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+import static com.ssafy.luminous.config.BaseResponseStatus.SUCCESS;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/places")
@@ -23,47 +26,64 @@ public class PlaceController {
     private final JwtService jwtService;
 
     @GetMapping("")
-    public List<PlaceListResDto> getPlaceList(
+    public BaseResponse<List> getPlaceList(
             // keyword {place, user, desc}
             @RequestParam(value = "category", defaultValue = "place") String category,
             @RequestParam(value = "keyword", defaultValue = "") String keyword
-            ){
+    ) {
+        try {
+            return new BaseResponse<>(placeService.getPlaceList(category, keyword));
 
-        return placeService.getPlaceList(category, keyword);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
 
     }
 
     @GetMapping("/{id}")
-    public BaseResponse<Place> getPlace(@PathVariable Long id){
+    public BaseResponse<Place> getPlace(@PathVariable Long id) {
         try {
             placeService.updateHit(id);
-            return new BaseResponse<>(placeService.getPlace(id)) ;
-        }
-        catch (BaseException exception){
+            return new BaseResponse<>(placeService.getPlace(id));
+        } catch (BaseException exception) {
             return new BaseResponse<>(exception.getStatus());
         }
 
     }
 
     @PostMapping("")
-    public Place postPlace(HttpServletRequest request , @RequestBody PlacePostReqDto placePostReqDto){
+    public BaseResponse<Place> postPlace(HttpServletRequest request, @RequestBody PlacePostReqDto placePostReqDto) {
 //        Long memberId = jwtService.getIdFromToken(request);
 //        return placeService.postPlace(placePostReqDto,memberId);
-        return placeService.postPlace(placePostReqDto);
+        try {
+
+            return new BaseResponse<>(placeService.postPlace(placePostReqDto));
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
     }
+
     @DeleteMapping("/{id}")
-    public Boolean deletePlace(HttpServletRequest request ,@PathVariable Long id){
+    public BaseResponse<?> deletePlace(HttpServletRequest request, @PathVariable Long id) {
 //        Long memberId = jwtService.getIdFromToken(request);
 //        placeService.deletePlace(id,memberId);
-        placeService.deletePlace(id);
-        return true;
+        try {
+            placeService.deletePlace(id);
+            return new BaseResponse<>(SUCCESS);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
     }
 
     @PutMapping("/{id}")
-    public Place updatePlace(HttpServletRequest request ,@PathVariable Long id, @RequestBody PlaceUpdateReqDto placeUpdateReqDto){
+    public BaseResponse<Place> updatePlace(HttpServletRequest request, @PathVariable Long id, @RequestBody PlaceUpdateReqDto placeUpdateReqDto) {
 //        Long memberId = jwtService.getIdFromToken(request);
 //        return placeService.updatePlace(id, placeUpdateReqDto,memberId);
-        return placeService.updatePlace(id, placeUpdateReqDto);
+        try {
+            return new BaseResponse<>(placeService.updatePlace(id, placeUpdateReqDto));
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
     }
 
 
--- src/main/java/com/ssafy/luminous/place/service/PlaceService.java
+++ src/main/java/com/ssafy/luminous/place/service/PlaceService.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static com.ssafy.luminous.config.BaseResponseStatus.DATABASE_ERROR;
 import static com.ssafy.luminous.config.BaseResponseStatus.NOT_OWNER;
 
 @Service
@@ -23,75 +24,93 @@ public class PlaceService {
 
     private final PlaceRepository placeRepository;
 
-    public List<PlaceListResDto> getPlaceList(String category, String keyword){
+    public List<PlaceListResDto> getPlaceList(String category, String keyword) throws BaseException {
         List<Place> places;
 
-        List<PlaceListResDto> newPlaces =  new ArrayList<>();
+        List<PlaceListResDto> newPlaces = new ArrayList<>();
 
-        // 유저이름
-        if("user".equals(category)){
-            places = placeRepository.findByMember_MemberNameContains(keyword);
-        }
-        // 설명
-        else if ("desc".equals(category)) {
-            places= placeRepository.findByPlaceDescriptionContains(keyword);
-        }
-        // 장소이름
-        else {
-            places = placeRepository.findByPlaceNameContains(keyword);
-        }
+        try {
+            // 유저이름
+            if ("user".equals(category)) {
+                places = placeRepository.findByMember_MemberNameContains(keyword);
+            }
+            // 설명
+            else if ("desc".equals(category)) {
+                places = placeRepository.findByPlaceDescriptionContains(keyword);
+            }
+            // 장소이름
+            else {
+                places = placeRepository.findByPlaceNameContains(keyword);
+            }
 
-        for(Place place : places){
-            newPlaces.add(PlaceListResDto.builder()
-                    .id(place.getId())
-                    .placeName(place.getPlaceName())
-                    .placeDescription(place.getPlaceDescription())
-                    .hit(place.getHit())
-                    .createdDate(place.getCreatedDate())
-                    .rate(place.getRate())
-                    .member(place.getMember())
-                    .build());
+            for (Place place : places) {
+                newPlaces.add(PlaceListResDto.builder()
+                        .id(place.getId())
+                        .placeName(place.getPlaceName())
+                        .placeDescription(place.getPlaceDescription())
+                        .hit(place.getHit())
+                        .createdDate(place.getCreatedDate())
+                        .rate(place.getRate())
+                        .member(place.getMember())
+                        .build());
+            }
+        } catch (Exception e) {
+            throw new BaseException(DATABASE_ERROR);
         }
 
         return newPlaces;
     }
 
-    public Place postPlace(PlacePostReqDto placePostReqDto, Long memberId){
-           return placeRepository.save(placePostReqDto.toEntity(memberId));
+    public Place postPlace(PlacePostReqDto placePostReqDto, Long memberId) throws BaseException {
+        try {
+            return placeRepository.save(placePostReqDto.toEntity(memberId));
+        } catch (Exception e) {
+            throw new BaseException(DATABASE_ERROR);
+        }
     }
-    public Place postPlace(PlacePostReqDto placePostReqDto){
-        return placeRepository.save(placePostReqDto.toEntity(1L));
+
+    public Place postPlace(PlacePostReqDto placePostReqDto) throws BaseException {
+        try {
+            return placeRepository.save(placePostReqDto.toEntity(1L));
+        } catch (Exception e) {
+            throw new BaseException(DATABASE_ERROR);
+        }
     }
-    public void deletePlace(Long id) {
+
+    public void deletePlace(Long id) throws BaseException {
         Optional<Place> place = placeRepository.findById(id);
-        if(place.isEmpty()){
+        if (place.isEmpty()) {
             System.out.println("사용자의 게시글이 아니에요~");
-            throw new RuntimeException("사용자 게시글이 아녜요");
+            throw new BaseException(NOT_OWNER);
         }
         placeRepository.deleteById(id);
     }
-    public void deletePlace(Long id, Long memberId) {
-        Optional<Place> place = placeRepository.findByIdAndMember_id(id,memberId);
-        if(place.isEmpty()){
+
+    public void deletePlace(Long id, Long memberId) throws BaseException {
+        Optional<Place> place = placeRepository.findByIdAndMember_id(id, memberId);
+        if (place.isEmpty()) {
             System.out.println("사용자의 게시글이 아니에요~");
-            throw new RuntimeException("사용자 게시글이 아녜요");
+            throw new BaseException(NOT_OWNER);
         }
         placeRepository.deleteById(id);
     }
-    public Place updatePlace(Long id, PlaceUpdateReqDto placeUpdateReqDto){
+
+    public Place updatePlace(Long id, PlaceUpdateReqDto placeUpdateReqDto) throws BaseException {
         Optional<Place> place = placeRepository.findById(id);
-        if(place.isEmpty()){
+
+        if (place.isEmpty()) {
             System.out.println("사용자의 게시글이 아니에요~");
-            throw new RuntimeException("사용자 게시글이 아녜요");
+            throw new BaseException(NOT_OWNER);
         }
         place.get().update(placeUpdateReqDto);
         return place.get();
 
     }
+
     public Place updatePlace(Long id, PlaceUpdateReqDto placeUpdateReqDto, Long memberId) throws BaseException {
 
-        Optional<Place> place = placeRepository.findByIdAndMember_id(id,memberId);
-        if(place.isEmpty()){
+        Optional<Place> place = placeRepository.findByIdAndMember_id(id, memberId);
+        if (place.isEmpty()) {
             System.out.println("사용자의 게시글이 아니에요~");
             throw new BaseException(NOT_OWNER);
         }
@@ -102,8 +121,8 @@ public class PlaceService {
     }
 
     public Place getPlace(Long id) throws BaseException {
-        Optional<Place> place =placeRepository.findById(id);
-        if(place.isEmpty()){
+        Optional<Place> place = placeRepository.findById(id);
+        if (place.isEmpty()) {
             throw new BaseException(NOT_OWNER);
         }
         return place.get();
